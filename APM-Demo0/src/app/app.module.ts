738c61a9ecf35ae3c58d1d0567cabe79c2cf8c96
epaphras roamba <eroamba@hotmail.com>
2019-11-23 14:33:22 +0100

    Initial project

---
+++ APM-Demo0/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {StoreModule, StoreRootModule} from '@ngrx/store';
+// Imports for loading & configuring the in-memory web api
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { ShellComponent } from './home/shell.component';
+import { MenuComponent } from './home/menu.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+
+/* Feature Modules */
+import { UserModule } from './user/user.module';
+import { reducer } from './products/state/product.reducer';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducer),
+    HttpClientInMemoryWebApiModule.forRoot(ProductData),
+    UserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    ShellComponent,
+    MenuComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
